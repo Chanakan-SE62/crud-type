2c7e9b719faa42c2d5e9021201db475bc90265e7
Chanakan-SE62 <624259009@webmail.npru.ac.th>
2022-03-13 19:29:34 +0700

    frontend index admin - success

---
+++ app/Views/components/card-data.php
@@ -0,0 +1,29 @@
+<div class="card-data">
+    <div class="card card-data-sub" id="card-data-1" style="width: 18rem;">
+        <div class="card-body card-data-body">
+            <h5 class="card-title card-data-title">ผู้ใช้งานตอนนี้</h5>
+            <h1 class="card-subtitle mb-2 text-muted">3,512 คน</h1>
+        </div>
+    </div>
+
+    <div class="card card-data-sub" id="card-data-2" style="width: 18rem;">
+        <div class="card-body card-data-body">
+            <h5 class="card-title card-data-title">ยืนยันตัวตนสำเร็จแล้ว</h5>
+            <h1 class="card-subtitle mb-2 text-muted">5,723 คน</h1>
+        </div>
+    </div>
+
+    <div class="card card-data-sub" id="card-data-3" style="width: 18rem;">
+        <div class="card-body card-data-body">
+            <h5 class="card-title card-data-title">รอการยืนยัน</h5>
+            <h1 class="card-subtitle mb-2 text-muted">420 คน</h1>
+        </div>
+    </div>
+
+    <div class="card card-data-sub" id="card-data-4" style="width: 18rem;">
+        <div class="card-body card-data-body">
+            <h5 class="card-title card-data-title">ผู้ใช้ใหม่ในสัปดาห์นี้</h5>
+            <h1 class="card-subtitle mb-2 text-muted">139 คน</h1>
+        </div>
+    </div>
+</div>
---
+++ app/Views/components/graph-data.php
@@ -0,0 +1,14 @@
+<style>
+    .img-graph1 {
+        width: 50%;
+        margin-top: 2%;
+        float: left;
+    }
+    .img-graph2 {
+        width: 50%;
+        margin-top: 2%;
+    }
+</style>
+
+<img src="https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.15752-9/274802646_291586679772095_1391211124209709641_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeFaeX0C32CCPtJ3kow6Fh9xb6UvSCPpTD1vpS9II-lMPRz8WJeetVL2Ehzs18kpFWGkzES9AW-cw3rAjRbFHK-l&_nc_ohc=8FGtwBMPiiwAX89ANko&_nc_ht=scontent.fbkk22-1.fna&oh=03_AVISv9e-tk_s5QMaWcMGbFWcLh_mWe8XvaHttj8oKuRLew&oe=6253811F" class="img-graph1">
+<img src="https://scontent.fbkk22-1.fna.fbcdn.net/v/t1.15752-9/274802646_291586679772095_1391211124209709641_n.png?_nc_cat=111&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeFaeX0C32CCPtJ3kow6Fh9xb6UvSCPpTD1vpS9II-lMPRz8WJeetVL2Ehzs18kpFWGkzES9AW-cw3rAjRbFHK-l&_nc_ohc=8FGtwBMPiiwAX89ANko&_nc_ht=scontent.fbkk22-1.fna&oh=03_AVISv9e-tk_s5QMaWcMGbFWcLh_mWe8XvaHttj8oKuRLew&oe=6253811F" class="img-graph2">
